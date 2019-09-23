b583cfe86207360868c090a1a2cfcd43dd3a2c3e
dario.zubaray <dzubaray@imagosur.com>
2019-09-23 09:25:52 -0300

    Añadiendo el modulo HttpClientModule, e injectando en el servicio el httpClient para realizar la consulta al servidor springboot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -25,7 +26,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sevices/cliente.service.ts
+++ src/app/sevices/cliente.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { Cliente } from '../components/clientes/cliente';
 import { CLIENTES } from '../components/clientes/clientes.json';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndpoint = 'http://localhost:8080/api'
+
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    const url = this.urlEndpoint + '/clientes';
+    return this.http.get<Cliente[]>(url);
+    // return of(CLIENTES);
   }
 }
