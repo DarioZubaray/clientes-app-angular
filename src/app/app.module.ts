1e0a100bbad4c85d7a0607a6565b236a7653a96a
dario.zubaray <dzubaray@imagosur.com>
2019-09-24 09:20:02 -0300

    declarando en el app.module el formModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -9,11 +10,14 @@ import { DirectivaComponent } from './components/directiva/directiva.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
 
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './components/clientes/form.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
-  {path: 'clientes', component: ClientesComponent},
-  {path: 'directiva', component: DirectivaComponent}
+  {path: 'clientes', component: ClientesComponent },
+  {path: 'directiva', component: DirectivaComponent },
+  {path: 'clientes/forma', component: FormComponent },
+  {path: 'clientes/forma/:id', component: FormComponent }
 ]
 
 @NgModule({
@@ -22,12 +26,14 @@ const routes: Routes = [
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
