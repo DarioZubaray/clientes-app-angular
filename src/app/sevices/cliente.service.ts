22170dd3a83a65537e0ef576a9fb89693c07d110
dario.zubaray <dzubaray@imagosur.com>
2019-09-24 09:21:03 -0300

    añadiendo al servicio, los metodos para crear y obtener por id

--- src/app/sevices/cliente.service.ts
+++ src/app/sevices/cliente.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from '../components/clientes/cliente';
-import { CLIENTES } from '../components/clientes/clientes.json';
-import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  private urlEndpoint = 'http://localhost:8080/api'
+  private urlEndpoint = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({'Content-type': 'application/json'})
 
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    const url = this.urlEndpoint + '/clientes';
-    return this.http.get<Cliente[]>(url);
+    return this.http.get<Cliente[]>(this.urlEndpoint);
     // return of(CLIENTES);
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndpoint, cliente, { headers: this.httpHeaders});
+  }
+
+  getCliente(id: number): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.urlEndpoint}/${id}`);
   }
 }
