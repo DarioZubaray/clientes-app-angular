0efb7c78515bff0d540331729537f2c4f0657c03
dario.zubaray <dzubaray@imagosur.com>
2019-09-25 09:32:36 -0300

    llamando a las funciones del service de clientes

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from '../../sevices/cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -20,4 +21,34 @@ export class ClientesComponent implements OnInit {
     });
   }
 
+  public delete(cliente: Cliente): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    });
+
+    swalWithBootstrapButtons.fire({
+      title: '¿Está seguro?',
+      text: `¿Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Si, ¡Borrarlo!',
+      cancelButtonText: 'No, ¡Cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.clientes = this.clientes.filter(cli => cli !== cliente);
+        this._clienteService.delete(cliente.id).subscribe( () => {
+          swalWithBootstrapButtons.fire(
+            '¡Borrado!',
+            'El cliente ${cliente.nombre} ${cliente.apellido} ha sido eliminado.',
+            'success'
+          );
+        });
+      }
+    });
+  }
 }
--- src/app/components/clientes/form.component.ts
+++ src/app/components/clientes/form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from '../../sevices/cliente.service';
-import { Router, ActivatedRouter } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -13,7 +13,11 @@ export class FormComponent implements OnInit {
 
   titulo: string = "Crear nuevo cliente";
   private cliente: Cliente = new Cliente();
-  constructor(private _clienteService: ClienteService, private router: Router, private activatedRouter: ActivatedRouter) { }
+
+  constructor(private _clienteService: ClienteService,
+              private router: Router,
+              private activatedRouter: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.cargarCliente();
@@ -23,9 +27,9 @@ export class FormComponent implements OnInit {
     this.activatedRouter.params.subscribe( params => {
       let id = params['id'];
       if (id) {
-        this._clienteService.getCliente(id).subscribe( (cliente) => this.cliente = cliente)
+        this._clienteService.getCliente(id).subscribe( cliente => this.cliente = cliente );
       }
-    })
+    });
   }
 
   public create(): void {
@@ -35,5 +39,12 @@ export class FormComponent implements OnInit {
           Swal.fire('Nuevo Cliente', `Cliente ${cliente.nombre} creado con éxito`, 'success');
           this.router.navigate(['clientes']);
         });
+  }
+
+  public update(): void {
+    this._clienteService.update(this.cliente).subscribe(cliente => {
+      Swal.fire('Cliente Actualizado', `Cliente ${cliente.nombre} actualizado con éxito`, 'success');
+      this.router.navigate(['clientes']);
+    });
   }
 }
