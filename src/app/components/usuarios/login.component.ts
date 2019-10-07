3fd75cc7792b710ba7028e702d9963cb758dfa62
dario.zubaray <dzubaray@imagosur.com>
2019-10-07 09:02:06 -0300

    Modelo Usuario, añadiendo el metodo login en el componente

--- src/app/components/usuarios/login.component.ts
+++ src/app/components/usuarios/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './usuario';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo: string = "Por favor inicia sesión";
-  constructor() { }
+  usuario: Usuario;
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit() {
   }
 
+  login(): void {
+    console.log(this.usuario);
+    if (this.usuario.username == null || this.usuario.password == null) {
+      swal('Error Login', '¡Usuario o contraseña vacios!', 'error');
+      return;
+    }
+  }
 }
